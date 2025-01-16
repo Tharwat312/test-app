f0526fbc16012fbf1f6763e87d44692ea41f0f00
tharwat312 <tharwata312@gmail.com>
2025-01-16 17:50:15 +0200

    Add Home Image

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Heading from '../Heading/Heading'
-
+import avatarImage from '../../assets/images/Avatar.svg';
 function Home() {
     return (
         <>
+            <img src={avatarImage} className='w-1/6 mx-auto' alt="" />
             <Heading text={"start framework"} />
-            <div>Home</div>
         </>
     )
 }
